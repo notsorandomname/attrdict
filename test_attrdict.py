8ff2146c562b1093bc47dfdb45153efc31c6d302
RandomName <notsorandomname1@gmail.com>
2014-12-31 20:15:20 +0200

    Clean tests, remove copypaste, group tests

--- test_attrdict.py
+++ test_attrdict.py
@@ -1,83 +1,80 @@
 
 import pytest
+import mock
 from mock import MagicMock
 
 from attrdict import (
-    AttrDict as AD, PathTypeError, PathKeyError,
+    AttrDict, PathTypeError, PathKeyError,
     path_functor_wrapper
 )
 
-
-def test_empty_dict():
-    assert AD() == {}
+AD = AttrDict
 
 
-def test_update_like_behaviour():
-    assert AD(a=1, b=2) == {'a': 1, 'b': 2}
+@pytest.fixture
+def ad1():
+    return AD(root=1)
 
 
-def test_update_from_dict():
-    assert AD({'a': 1, 'b': 2}) == {'a': 1, 'b': 2}
+@pytest.fixture
+def ad2():
+    return AD(root=AD(leaf=2))
 
 
-def test_update_dict_is_copied():
-    x = {'a': 1, 'b': 2}
-    y = AD(x)
-    x['a'] = 3
-    assert x != y
+@pytest.fixture
+def ad3():
+    return AD(root=AD(branch=AD(leaf=3)))
 
 
-def test_update_dict_attrdict_is_copied():
-    x = AD({'a': 1, 'b': 2})
-    y = AD(x)
-    y['a'] = 3
-    assert x != y
+class TestBasicProperties(object):
+    def test_empty_dict(self):
+        assert AD() == {}
 
+    def test_update_like_behaviour(self):
+        assert AD(a=1, b=2) == {'a': 1, 'b': 2}
 
-def test_setitem_dict_is_copied():
-    x = AD()
-    y = AD(a=1)
-    x['dict'] = y
-    y['b'] = 2
-    assert x['dict'] != y
+    def test_update_from_dict(self):
+        assert AD({'a': 1, 'b': 2}) == {'a': 1, 'b': 2}
 
+    def test_setattr_sets_item(self):
+        x = AD()
+        x.element = 1
+        assert x['element'] == 1
 
-def test_setattr_sets_item():
-    x = AD()
-    x.element = 1
-    assert x['element'] == 1
+    def test_getattr_gets_item(self):
+        x = AD(element=1)
+        assert x.element == 1
 
+    def test_getattr_raises_attribute_error(self):
+        x = AD(element=1)
+        with pytest.raises(AttributeError) as exc_info:
+            x.unknown
+        assert 'unknown' == exc_info.value[0]
 
-def test_getattr_gets_item():
-    x = AD(element=1)
-    assert x.element == 1
+    def test_repr(self):
+        x = AD(element=1)
+        assert repr(x) == str(x) == "AttrDict({'element': 1})"
 
+    # Test that AttrDict doesn't share subdicts with another AttrDicts
 
-def test_getattr_raises_attribute_error():
-    x = AD(element=1)
-    with pytest.raises(AttributeError) as exc_info:
-        x.unknown
-    assert 'unknown' == exc_info.value[0]
+    def test_update_dict_is_copied(self):
+        x = {'a': 1, 'b': 2}
+        y = AD(x)
+        x['a'] = 3
+        assert x != y
 
+    def test_update_dict_attrdict_is_copied(self):
+        x = AD({'a': 1, 'b': 2})
+        y = AD(x)
+        y['a'] = 3
+        assert x != y
 
-def test_repr():
-    x = AD(element=1)
-    assert repr(x) == str(x) == "AttrDict({'element': 1})"
-
-
-def test_get_mapping_empty():
-    x = AD(element=1)
-    assert x._get_mapping(tuple()) is x
-
-
-def test_get_mapping_depth_1():
-    x = AD(root=AD(leaf=1))
-    assert x._get_mapping(tuple(['root'])) is x.root
-
-
-def test_get_mapping_depth_2():
-    x = AD(root=AD(branch=AD(leaf=1)))
-    assert x._get_mapping(tuple(['root', 'branch'])) is x.root.branch
+    def test_setitem_dict_is_copied(self):
+        x = AD()
+        y = AD(a=1)
+        x['dict'] = y
+        y['b'] = 2
+        assert x['dict'] != y
 
 
 def assert_path_not_a_mapping_error(exc_info, path, key, full_path):
@@ -89,24 +86,6 @@ def assert_path_not_a_mapping_error(exc_info, path, key, full_path):
     assert ('expected mapping, got %s instead' % repr(int)) in exc[0]
 
 
-def test_get_mapping_not_a_mapping():
-    x = AD(root=1)
-    with pytest.raises(PathTypeError) as exc_info:
-        x._get_mapping(tuple(['root']))
-    assert_path_not_a_mapping_error(
-        exc_info, path=(), key='root', full_path=('root',)
-    )
-
-
-def test_get_mapping_not_a_mapping_depth_2():
-    x = AD(root=AD(leaf=1))
-    with pytest.raises(PathTypeError) as exc_info:
-        x._get_mapping(tuple(['root', 'leaf']))
-    assert_path_not_a_mapping_error(
-        exc_info, path=('root',), key='leaf', full_path=('root', 'leaf')
-    )
-
-
 def assert_path_key_error(exc_info, key, path, full_path):
     exc = exc_info.value
     details = exc[1]
@@ -115,239 +94,246 @@ def assert_path_key_error(exc_info, key, path, full_path):
     assert exc[0] == key
 
 
-def test_get_mapping_key_error():
-    x = AD()
-    path = tuple(['unknown'])
-    with pytest.raises(PathKeyError) as exc_info:
-        x._get_mapping(path)
-    assert_path_key_error(exc_info, 'unknown', path=(), full_path=path)
+class TestGetMapping(object):
+    def test_get_mapping_empty(self):
+        x = AD(element=1)
+        assert x._get_mapping(tuple()) is x
+
+    def test_get_mapping_depth_1(self, ad2):
+        assert ad2._get_mapping(tuple(['root'])) is ad2.root
+
+    def test_get_mapping_depth_2(self, ad3):
+        assert ad3._get_mapping(tuple(['root', 'branch'])) is ad3.root.branch
+
+    def test_get_mapping_not_a_mapping(self, ad1):
+        with pytest.raises(PathTypeError) as exc_info:
+            ad1._get_mapping(tuple(['root']))
+        assert_path_not_a_mapping_error(
+            exc_info, path=(), key='root', full_path=('root',)
+        )
+
+    def test_get_mapping_not_a_mapping_depth_2(self, ad2):
+        with pytest.raises(PathTypeError) as exc_info:
+            ad2._get_mapping(tuple(['root', 'leaf']))
+        assert_path_not_a_mapping_error(
+            exc_info, path=('root',), key='leaf', full_path=('root', 'leaf')
+        )
+
+    def test_get_mapping_key_error(self, ad1):
+        path = tuple(['unknown'])
+        with pytest.raises(PathKeyError) as exc_info:
+            ad1._get_mapping(path)
+        assert_path_key_error(exc_info, 'unknown', path=(), full_path=path)
+
+    def test_get_mapping_key_error_depth_2(self, ad2):
+        path = tuple(['root', 'unknown'])
+        with pytest.raises(PathKeyError) as exc_info:
+            ad2._get_mapping(path)
+        assert_path_key_error(exc_info, 'unknown',
+                              path=('root',), full_path=path)
+
+    def test_get_or_create_mapping_acts_as_get(self, ad2):
+        assert ad2._get_or_create_mapping(('root',)) is ad2.root
+
+    def test_get_or_create_mapping_creates_attrdict(self):
+        x = AD()
+        m = x._get_or_create_mapping(('root', 'branch'))
+        assert x.root.branch is m
+        assert isinstance(m, type(x))
+
+    def test_get_or_create_mapping_fails_to_create_attrdict(self, ad2):
+        path = ('root', 'leaf')
+        with pytest.raises(TypeError) as exc_info:
+            ad2._get_or_create_mapping(path)
+        assert_path_not_a_mapping_error(
+            exc_info, path=('root',),
+            key='leaf', full_path=('root', 'leaf')
+        )
 
 
-def test_get_mapping_key_error_depth_2():
-    x = AD(root=AD(leaf=1))
-    path = tuple(['root', 'unknown'])
-    with pytest.raises(PathKeyError) as exc_info:
-        x._get_mapping(path)
-    assert_path_key_error(exc_info, 'unknown', path=('root',), full_path=path)
+class TestGetPath(object):
+    def test_depth_1(self, ad1):
+        assert ad1.get_path(('root',)) == 1
+
+    def test_depth_1_default(self, ad1):
+        assert ad1.get_path(('unknown',), 'default') == 'default'
+
+    def test_depth_2(self, ad2):
+        assert ad2.get_path(('root', 'leaf')) == 2
+
+    def test_depth_2_default(self, ad2):
+        assert ad2.get_path(('root', 'unknown'), 'default') == 'default'
+
+    def test_depth_3(self, ad3):
+        assert ad3.get_path(('root', 'branch', 'leaf')) == 3
+
+    def test_depth_1_none_on_default(self, ad1):
+        assert ad1.get_path(('unknown',)) is None
+
+    def test_depth_2_none_on_default(self, ad2):
+        assert ad2.get_path(('root', 'unknown')) is None
 
 
-def test_get_or_create_mapping_acts_as_get():
-    x = AD(root=AD(leaf=1))
-    assert x._get_or_create_mapping(('root',)) is x.root
+class CheckPathCalled(Exception):
+    pass
 
 
-def test_get_or_create_mapping_creates_attrdict():
-    x = AD()
-    m = x._get_or_create_mapping(('root', 'branch'))
-    assert x.root.branch is m
-    assert isinstance(m, type(x))
+class TestCheckPath(object):
+    all_methods_decorator = pytest.mark.parametrize("func,additional_args", [
+        ("get_path", []),
+        ("set_path", [42]),
+        ("setdefault_path", [42]),
+        (("pop_path", [42])),
+    ])
+
+    @all_methods_decorator
+    def test_check_path_called(self, ad1, func, additional_args):
+        with mock.patch('attrdict.AttrDict._check_path') as patched_check_path:
+            patched_check_path.side_effect = CheckPathCalled()
+            with pytest.raises(CheckPathCalled):
+                getattr(ad1, func)(None, *additional_args)
+
+    def test_path_not_a_tuple(self):
+        with pytest.raises(TypeError) as exc_info:
+            AD._check_path(42)
+        assert 'expected tuple or list, got %r instead' % int in str(exc_info)
+
+    def test_path_empty_tuple(self):
+        with pytest.raises(ValueError) as exc_info:
+            AD._check_path(())
+        assert 'path is empty' in str(exc_info)
+
+    def test_path_empty_tuple_allow_empty(self):
+        AD._check_path((), allow_empty=True)
+
+    def test_path_unhashable(self):
+        with pytest.raises(TypeError):
+            AD._check_path(('a', 'b', []))
 
 
-def test_get_or_create_mapping_fails_to_create_attrdict():
-    x = AD(root=AD(branch=1))
-    path = ('root', 'branch')
-    with pytest.raises(TypeError) as exc_info:
-        x._get_or_create_mapping(path)
-    assert_path_not_a_mapping_error(
-        exc_info, path=('root',), key='branch', full_path=('root', 'branch')
+class TestSetPath(object):
+    set_and_setdefault = pytest.mark.parametrize(
+        "method", ['set_path', 'setdefault_path']
     )
 
+    @set_and_setdefault
+    def test_depth_1(self, method):
+        x = AD()
+        getattr(x, method)(('root',), 5)
+        assert x.root == 5
 
-def test_get_path_depth_1():
-    x = AD(element=1)
-    assert x.get_path(('element',)) == 1
+    @set_and_setdefault
+    def test_depth_2(self, method):
+        x = AD()
+        getattr(x, method)(('root', 'leaf'), 5)
+        assert x.root.leaf == 5
 
+    @set_and_setdefault
+    def test_existing_non_branch(self, method, ad2):
+        path = ('root', 'leaf', 'unknown')
+        with pytest.raises(TypeError) as exc_info:
+            getattr(ad2, method)(path, 2)
+        assert_path_not_a_mapping_error(
+            exc_info, path=('root',), key='leaf', full_path=path
+        )
 
-def test_get_path_depth_1_default():
-    x = AD(element=1)
-    assert x.get_path(('unknown',), 1) == 1
+    @set_and_setdefault
+    def test_depth_existing_branch(self, method, ad3):
+        getattr(ad3, method)(('root', 'branch', 'another'), 2)
+        assert ad3.root.branch.another == 2
 
+    def test_set_changes_existing_value(self, ad2):
+        ad2.set_path(('root', 'leaf'), 'new')
+        assert ad2.root.leaf == 'new'
 
-def test_get_path_depth_2():
-    x = AD(root=AD(leaf=1))
-    assert x.get_path(('root', 'leaf')) == 1
+    def test_setdefault_doesnt_change_existing_value(self, ad2):
+        ad2.setdefault_path(('root', 'leaf'), 42)
+        assert ad2.root.leaf == 2
 
+    def test_setdefault_returns_current_value(self, ad2):
+        assert ad2.setdefault_path(('root', 'leaf'), 42) == 2
 
-def test_get_path_depth_2_default():
-    x = AD(root=AD(leaf=1))
-    assert x.get_path(('root', 'unknown'), 1) == 1
+    def test_setdefault_sets_none_on_default(self):
+        ad = AD()
+        assert ad.setdefault_path(('unknown',)) is None
+        assert ad.unknown is None
 
+    def test_setdefault_sets_none_on_default_depth_2(self, ad2):
+        assert ad2.setdefault_path(('root', 'unknown')) is None
+        assert ad2.root.unknown is None
 
-def test_get_path_returns_none_on_default():
-    x = AD()
-    assert x.get_path(('unknown',)) is None
-
-
-@pytest.mark.parametrize("func,additional_args", [
-    ("get_path", []),
-    ("set_path", [42]),
-    ("setdefault_path", [42]),
-    (("pop_path", [42])),
-])
-def test_empty_path_raises_value_error(func, additional_args):
-    x = AD(value=1)
-    with pytest.raises(ValueError) as exc_info:
-        getattr(x, func)(tuple(), *additional_args)
-    assert 'path is empty' == exc_info.value[0]
-
-
-def test_check_path_not_a_tuple():
-    with pytest.raises(TypeError) as exc_info:
-        AD._check_path(42)
-    assert 'expected tuple or list, got %s instead' % repr(int) in str(exc_info)
-
-
-def test_check_path_empty_tuple():
-    with pytest.raises(ValueError) as exc_info:
-        AD._check_path(())
-    assert 'path is empty' in str(exc_info)
-
-
-def test_check_path_empty_tuple_allow_empty():
-    AD._check_path((), allow_empty=True)
+    def test_setdefault_doesnt_change_existing_value_on_default(self, ad2):
+        assert ad2.setdefault_path(('root', 'leaf')) == 2
+        assert ad2.root.leaf == 2
 
 
-def test_check_path_unhashable():
-    with pytest.raises(TypeError):
-        AD._check_path(('a', 'b', []))
+class TestPopPath(object):
+    def test_existing_depth_1(self, ad1):
+        assert ad1.pop_path(('root',)) == 1
+        assert ad1 == AD()
+
+    def test_existing_depth_2(self, ad2):
+        assert ad2.pop_path(('root', 'leaf')) == 2
+        assert ad2 == AD(root=AD())
+
+    def test_non_existing_raises_key_error_depth_1(self, ad1):
+        ad1_copy = AD(ad1)
+        with pytest.raises(PathKeyError) as exc_info:
+            ad1.pop_path(('unknown',))
+        assert ad1 == ad1_copy
+        assert_path_key_error(exc_info, 'unknown', path=(),
+                              full_path=('unknown',))
+
+    def test_non_existing_raises_key_error_depth_2(self, ad2):
+        ad2_copy = AD(ad2)
+        with pytest.raises(PathKeyError) as exc_info:
+            ad2.pop_path(('root', 'unknown'))
+        assert ad2 == ad2_copy
+        assert_path_key_error(exc_info, 'unknown', path=('root',),
+                              full_path=('root', 'unknown',))
+
+    def test_non_existing_returns_default_value(self, ad1):
+        ad1_copy = AD(ad1)
+        assert ad1.pop_path(('unknown',), 42) == 42
+        assert ad1 == ad1_copy
+
+    def test_non_existing_depth_2_returns_default_value(self, ad2):
+        ad2_copy = AD(ad2)
+        assert ad2.pop_path(('root', 'unknown'), 42) == 42
+        assert ad2 == ad2_copy
+
+    def test_non_existing_intermediate_branch_raises_key_error(self, ad3):
+        ad3_copy = AD(ad3)
+        path = ('root', 'unknown', 'leaf')
+        with pytest.raises(PathKeyError) as exc_info:
+            ad3.pop_path(path)
+        assert ad3 == ad3_copy
+        assert_path_key_error(exc_info, 'unknown', path=('root',),
+                              full_path=path)
+
+    def test_non_existing_intermediate_branch_returns_default_value(self, ad3):
+        ad3_copy = AD(ad3)
+        assert ad3.pop_path(('root', 'unknown', 'leaf'), 42) == 42
+        assert ad3 == ad3_copy
 
 
-@pytest.mark.parametrize("method", ['set_path', 'setdefault_path'])
-def test_set_path(method):
-    x = AD()
-    getattr(x, method)(('root',), 5)
-    assert x.root == 5
+class TestMagicSyntax(object):
+    def test_get(self, ad3):
+        assert ad3.get.root() is ad3.root
 
+    def test_get_default_none(self, ad3):
+        assert ad3.get.root.unknown() is None
 
-@pytest.mark.parametrize("method", ['set_path', 'setdefault_path'])
-def test_set_path_depth_2(method):
-    x = AD()
-    getattr(x, method)(('root', 'leaf'), 5)
-    assert x.root.leaf == 5
+    def test_get_default(self, ad3):
+        assert ad3.get.root.unknown('default') == 'default'
 
+    def test_set_setattr(self, ad1):
+        ad1.set.another = 1
+        assert ad1 == AD(root=1, another=1)
 
-@pytest.mark.parametrize("method", ['set_path', 'setdefault_path'])
-def test_set_path_existing_non_branch(method):
-    x = AD(root=AD(branch=1))
-    path = ('root', 'branch', 'leaf')
-    with pytest.raises(TypeError) as exc_info:
-        getattr(x, method)(('root', 'branch', 'leaf'), 2)
-    assert_path_not_a_mapping_error(
-        exc_info, path=('root',), key='branch', full_path=path
-    )
-
-
-@pytest.mark.parametrize("method", ['set_path', 'setdefault_path'])
-def test_set_path_depth_existing_branch(method):
-    x = AD(root=AD(branch=AD(leaf=1)))
-    getattr(x, method)(('root', 'branch', 'another'), 2)
-    assert x.root.branch.another == 2
-
-
-def test_set_path_changes_existing_value():
-    x = AD(root=AD(leaf=1))
-    x.set_path(('root', 'leaf'), 2)
-    assert x.root.leaf == 2
-
-
-def test_setdefault_path_doesnt_change_existing_value():
-    x = AD(root=AD(leaf=1))
-    x.setdefault_path(('root', 'leaf'), 2)
-    assert x.root.leaf == 1
-
-
-def test_setdefault_path_returns_current_value():
-    x = AD(root=AD(leaf=1))
-    assert x.setdefault_path(('root', 'leaf'), 2) == 1
-
-
-def test_setdefault_path_sets_none_on_default():
-    x = AD()
-    assert x.setdefault_path(('unknown',)) is None
-    assert x.unknown is None
-
-
-def test_pop_path_simple():
-    x = AD(root=1)
-    assert x.pop_path(('root',)) == 1
-    assert x == AD()
-
-
-def test_pop_path_non_existing_raises_key_error():
-    x = AD(root=1)
-    y = AD(x)
-    with pytest.raises(PathKeyError) as exc_info:
-        x.pop_path(('unknown',))
-    assert x == y
-    assert_path_key_error(exc_info, 'unknown', path=(), full_path=('unknown',))
-
-
-def test_pop_path_non_existing_returns_default_value():
-    x = AD(root=1)
-    y = AD(x)
-    assert x.pop_path(('unknown',), 42) == 42
-    assert x == y
-
-
-def test_pop_path_non_existing_depth_2_raises_key_error():
-    x = AD(root=AD(leaf=1))
-    y = AD(x)
-    path = ('root', 'branch')
-    with pytest.raises(PathKeyError) as exc_info:
-        x.pop_path(('root', 'branch'))
-    assert x == y
-    assert_path_key_error(exc_info, 'branch', path=('root',), full_path=path)
-
-
-def test_pop_path_non_existing_depth_2_returns_default_value():
-    x = AD(root=AD(leaf=1))
-    y = AD(x)
-    assert x.pop_path(('root', 'branch'), 42) == 42
-    assert x == y
-
-
-def test_pop_path_non_existing_intermediate_branch_raises_key_error():
-    x = AD(root=AD(branch=AD(leaf=1)))
-    y = AD(x)
-    path = ('root', 'unknown', 'leaf')
-    with pytest.raises(PathKeyError) as exc_info:
-        x.pop_path(path)
-    assert x == y
-    assert_path_key_error(exc_info, 'unknown', path=('root',), full_path=path)
-
-
-def test_pop_path_non_existing_intermediate_branch_returns_default_value():
-    x = AD(root=AD(branch=AD(leaf=1)))
-    y = AD(x)
-    assert x.pop_path(('root', 'unknown', 'leaf'), 42) == 42
-    assert x == y
-
-
-def test_magic_syntax_get():
-    x = AD(root=AD(branch=AD(leaf=1)))
-    assert x.get.root() is x.root
-
-
-def test_magic_syntax_get_default_none():
-    x = AD(root=AD(branch=AD(leaf=1)))
-    assert x.get.root.unknown() is None
-
-
-def test_magic_syntax_get_default():
-    x = AD(root=AD(branch=AD(leaf=1)))
-    assert x.get.root.unknown('default') == 'default'
-
-
-def test_magic_syntax_set_setattr():
-    x = AD(root=1)
-    x.set.another = 1
-    assert x == AD(root=1, another=1)
-
-
-def test_magic_syntax_set_setattr_depth():
-    x = AD(root=1)
-    x.set.another.one = 1
-    assert x == AD(root=1, another=AD(one=1))
+    def test_set_setattr_depth(self, ad1):
+        ad1.set.another.one = 1
+        assert ad1 == AD(root=1, another=AD(one=1))
 
 
 class TestAttributePathAccessWrapper(object):
